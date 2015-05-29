162f30ad2a80b36a8e22c65b934ccab9b07ec6bf
emag <lanabe.lanabe@gmail.com>
2015-05-29 11:18:05 +0900

    Apply latest wildfly-swarm(1357ffd6440239dfceb024006f72c1311f0a36f8)

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ WildFly-Swarm Quick Starts
 * jaxrs
  * Basic JAX-RS sample
 * jaxrs-arquillian-cube
- * JAX-RS and Arquillian Cube Testing
+ * JAX-RS and Arquillian Cube Integration Testing
 * jaxrs-cdi
  * JAX-RS + CDI sample
+* jaxrs-cdi-jpa
+ * JAX-RS + CDI + JPA sample
--- jaxrs-cdi-jpa/src/main/java/wildflyswarm/jaxrsjpa/App.java
+++ jaxrs-cdi-jpa/src/main/java/wildflyswarm/jaxrsjpa/App.java
@@ -1,16 +1,17 @@
 package wildflyswarm.jaxrsjpa;
 
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.container.Container;
 import org.wildfly.swarm.datasources.Datasource;
 import org.wildfly.swarm.datasources.DatasourcesFraction;
 import org.wildfly.swarm.datasources.Driver;
-import org.wildfly.swarm.jaxrs.JaxRsDeployment;
+import org.wildfly.swarm.jaxrs.JAXRSDeployment;
 import wildflyswarm.jaxrsjpa.api.EmployeeController;
 import wildflyswarm.jaxrsjpa.domain.model.Employee;
 import wildflyswarm.jaxrsjpa.domain.repository.EmployeeRepository;
 import wildflyswarm.jaxrsjpa.domain.service.EmployeeService;
 
-/**
+/**g
  * @author Yoshimasa Tanabe
  */
 public class App {
@@ -26,15 +27,21 @@ public class App {
       .datasource(new Datasource("ExampleDS")
           .driver("h2")
           .connectionURL("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE")
-          .authentication("sa", "sa")
-      ));
+          .authentication("sa", "sa"))
+    );
 
-    JaxRsDeployment jaxRsDeployment = new JaxRsDeployment();
-    jaxRsDeployment
-      .addResource(Employee.class)
-      .addResource(EmployeeController.class)
-      .addResource(EmployeeService.class)
-      .addResource(EmployeeRepository.class);
+    container.start();
+
+    JAXRSDeployment jaxRsDeployment = new JAXRSDeployment(container);
+    jaxRsDeployment.addResource(EmployeeController.class);
+    jaxRsDeployment.getArchive().addClasses(
+      Employee.class,
+      EmployeeService.class,
+      EmployeeRepository.class);
+    jaxRsDeployment.getArchive().addAsWebInfResource(
+      new ClassLoaderAsset("META-INF/persistence.xml", App.class.getClassLoader()), "classes/META-INF/persistence.xml");
+    jaxRsDeployment.getArchive().addAsWebInfResource(
+      new ClassLoaderAsset("import.sql", App.class.getClassLoader()), "classes/import.sql");
 
     container.deploy(jaxRsDeployment);
   }
