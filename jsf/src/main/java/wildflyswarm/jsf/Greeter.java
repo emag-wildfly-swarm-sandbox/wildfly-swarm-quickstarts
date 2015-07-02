693c5f070aca77ed7b05aca950e07d51fa3e7118
emag <lanabe.lanabe@gmail.com>
2015-07-03 05:06:33 +0900

    Add JSF sample(WIP)

---
+++ jsf/src/main/java/wildflyswarm/jsf/Greeter.java
@@ -0,0 +1,15 @@
+package wildflyswarm.jsf;
+
+import javax.faces.bean.ManagedBean;
+
+/**
+ * @author Yoshimasa Tanabe
+ */
+@ManagedBean
+public class Greeter {
+
+  public String message() {
+    return "Howdy from JSF";
+  }
+
+}
